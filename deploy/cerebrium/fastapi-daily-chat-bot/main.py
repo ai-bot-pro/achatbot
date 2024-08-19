794b7277ba98242aceddc35ecdcd1ba9a0e2393e
weedge <weege007@gmail.com>
2024-08-19 21:08:15 +0800

    cerebrium/fastapi-daily-chat-bot main.py use achatbot serve method
    Signed-off-by: weedge <weege007@gmail.com>

--- deploy/cerebrium/fastapi-daily-chat-bot/main.py
+++ deploy/cerebrium/fastapi-daily-chat-bot/main.py
@@ -1,34 +1,22 @@
 from achatbot.cmd.http.server import fastapi_daily_bot_serve as serve
-from achatbot.cmd.http.server.fastapi_daily_bot_serve import BotInfo
 
-from cerebrium import get_secret
-
-
-def run(param_1: str, param_2: str, run_id):  # run_id is optional, injected by Cerebrium at runtime
-    my_results = {"1": param_1, "2": param_2}
-    my_status_code = 200  # if you want to return a specific status code
-
-    return {"my_result": my_results, "status_code": my_status_code}  # return your results
-
-def register_bot(bot_name: str = "DummyBot"):
-    return serve.register_bot(bot_name)
+# from cerebrium import get_secret
 
 
-async def bot_join(chat_bot_name: str, info: BotInfo):
-    res = await serve.bot_join(chat_bot_name, info)
-    return res
+def status():
+    return {"status_code": 200}
 
 
-async def bot_join_room(room_name: str, chat_bot_name: str, info: BotInfo):
-    res = await serve.bot_join_room(room_name, chat_bot_name, info)
-    return res
+# !TIPS:
+# cerebrum wrap fastapi, use HTTP POST method api
+# so use achatbot serve APIResponse dict[str, Any] to return
+# KISS, have a nice code :)
 
-
-async def create_random_room():
-    res = await serve.create_random_room()
-    return res
-
-
-async def get_status(pid: int):
-    res = await serve.get_status(pid)
-    return res
+create_random_room = serve.create_random_room
+register_bot = serve.register_bot
+bot_join = serve.bot_join
+bot_join_room = serve.bot_join_room
+create_random_room = serve.create_random_room
+get_bot_proc_status = serve.get_status
+get_num_bots = serve.get_num_bots
+get_room_bots = serve.get_room_bots
