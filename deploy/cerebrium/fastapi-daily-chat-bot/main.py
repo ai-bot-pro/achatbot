ffce78b491bcd605b60fe899fb318595696ebb90
weedge <weege007@gmail.com>
2024-08-21 19:34:55 +0800

    fix: dependencies pynini==2.1.5; sys_platform == 'linux', WeTextProcessing==1.0.2; sys_platform == 'linux', nemo_text_processing lastest un dep pynini 2.1.6
    Signed-off-by: weedge <weege007@gmail.com>

--- deploy/cerebrium/fastapi-daily-chat-bot/main.py
+++ deploy/cerebrium/fastapi-daily-chat-bot/main.py
@@ -6,17 +6,28 @@ from cerebrium import get_secret
 
 os.environ['LLM_CHAT_SYSTEM'] = get_secret("LLM_CHAT_SYSTEM")
 os.environ['OPENAI_API_KEY'] = get_secret("OPENAI_API_KEY")
+os.environ['GROQ_API_KEY'] = get_secret("GROQ_API_KEY")
+os.environ['TOGETHER_API_KEY'] = get_secret("TOGETHER_API_KEY")
 os.environ['DAILY_API_KEY'] = get_secret("DAILY_API_KEY")
 os.environ['CARTESIA_API_KEY'] = get_secret("CARTESIA_API_KEY")
 os.environ['DEEPGRAM_API_KEY'] = get_secret("DEEPGRAM_API_KEY")
 os.environ['ELEVENLABS_API_KEY'] = get_secret("ELEVENLABS_API_KEY")
 os.environ['JINA_API_KEY'] = get_secret("JINA_API_KEY")
+
 os.environ['VAD_ANALYZER_TAG'] = get_secret("VAD_ANALYZER_TAG")
+
 os.environ['ASR_TAG'] = get_secret("ASR_TAG")
 os.environ['ASR_LANG'] = get_secret("ASR_LANG")
 os.environ['ASR_MODEL_NAME_OR_PATH'] = get_secret("ASR_MODEL_NAME_OR_PATH")
+
+os.environ['LLM_OPENAI_BASE_URL'] = get_secret("LLM_OPENAI_BASE_URL")
+os.environ['LLM_OPENAI_MODEL'] = get_secret("LLM_OPENAI_MODEL")
+os.environ['LLM_LANG'] = get_secret("LLM_LANG")
+
 os.environ['TTS_TAG'] = get_secret("TTS_TAG")
 os.environ['TTS_LANG'] = get_secret("TTS_LANG")
+os.environ['TTS_VOICE'] = get_secret("TTS_VOICE")
+
 os.environ['TIDB_HOST'] = get_secret("TIDB_HOST")
 os.environ['TIDB_PORT'] = get_secret("TIDB_PORT")
 os.environ['TIDB_SSL_CA'] = get_secret("TIDB_SSL_CA")
