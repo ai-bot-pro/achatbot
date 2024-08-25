58b54de0dd541f30f7b7db55c12165f192665704
weedge <weege007@gmail.com>
2024-08-25 10:12:32 +0800

    feat: logging add trace att
    Signed-off-by: weedge <weege007@gmail.com>

--- src/common/logger.py
+++ src/common/logger.py
@@ -11,6 +11,37 @@ def getDefaultFormatter():
     return formatter
 
 
+def addLoggingLevel(levelName, levelNum, methodName=None):
+    # Adopted from https://stackoverflow.com/a/35804945/1691778
+    # Adds a new logging method to the logging module
+
+    if not methodName:
+        methodName = levelName.lower()
+
+    if hasattr(logging, levelName):
+        raise AttributeError("{} already defined in logging module".format(levelName))
+    if hasattr(logging, methodName):
+        raise AttributeError("{} already defined in logging module".format(methodName))
+    if hasattr(logging.getLoggerClass(), methodName):
+        raise AttributeError("{} already defined in logger class".format(methodName))
+
+    def logForLevel(self, message, *args, **kwargs):
+        if self.isEnabledFor(levelNum):
+            self._log(levelNum, message, args, **kwargs)
+
+    def logToRoot(message, *args, **kwargs):
+        logging.log(levelNum, message, *args, **kwargs)
+
+    logging.addLevelName(levelNum, levelName)
+    setattr(logging, levelName, levelNum)
+    setattr(logging.getLoggerClass(), methodName, logForLevel)
+    setattr(logging, methodName, logToRoot)
+
+
+# Create the TRACE level
+addLoggingLevel("TRACE", logging.DEBUG - 5)
+
+
 class Logger():
     """
     set root or app logger at once
