6b2f71c6567dc4b247e906af55cddc3ac638f39c
weedge <weege007@gmail.com>
2024-10-22 12:47:34 +0800

    feat: gen podcast cover image with flux; upload audio/image file to aws/cloudflare with s3 protocol;  insert podcast meta info into cloudflare d1;
    Signed-off-by: weedge <weege007@gmail.com>

---
+++ demo/aws/upload.py
@@ -0,0 +1,84 @@
+import os
+import logging
+
+from tqdm import tqdm
+import boto3
+import typer
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv(override=True)
+
+app = typer.Typer()
+
+
+@app.command('get_url')
+def get_url(file_name: str):
+    bucket_url = os.getenv("S3_BUCKET_URL")
+    url = f"{bucket_url}/{file_name}"
+    logging.info(f"url:{url}")
+
+    return url
+
+
+@app.command('r2')
+def r2_upload(remote_folder: str, local_file: str) -> str:
+    account_id = os.getenv("CLOUDFLARE_ACCOUNT_ID")
+    access_key = os.getenv("CLOUDFLARE_ACCESS_KEY")
+    secret_key = os.getenv("CLOUDFLARE_SECRET_KEY")
+    region_name = os.getenv("CLOUDFLARE_REGION")
+    endpoint_url = f'https://{account_id}.r2.cloudflarestorage.com'
+    return upload(remote_folder, local_file,
+                  endpoint_url, access_key, secret_key, region_name)
+
+
+@app.command("s3")
+def upload(
+    remote_folder: str,
+    local_file: str,
+    endpoint_url: str = "",
+    access_key: str = "",
+    secret_key: str = "",
+    region_name: str = "",
+) -> str:
+    endpoint_url = endpoint_url or os.getenv("AWS_ENDPOINT_URL")
+    access_key = access_key or os.getenv("AWS_ACCESS_KEY")
+    secret_key = secret_key or os.getenv("AWS_SECRET_KEY")
+    region_name = region_name or os.getenv("AWS_REGION")
+    s3 = boto3.client(
+        service_name='s3',
+        endpoint_url=endpoint_url,
+        aws_access_key_id=access_key,
+        aws_secret_access_key=secret_key,
+        region_name=region_name,  # Must be one of: wnam, enam, weur, eeur, apac, auto
+    )
+    file_name = os.path.basename(local_file)
+    file_size = os.path.getsize(local_file)
+    logging.info(f"Name: {file_name} Size: {round(file_size/1024,2)} KB")
+
+    with tqdm(total=file_size, unit='B', unit_scale=True, desc=f'Uploading {file_name} to R2', ascii=True) as pbar:
+        with open(local_file, 'rb') as f:
+            s3.upload_fileobj(f, remote_folder, file_name,
+                              Callback=lambda x: pbar.update(x))
+    object_information = s3.head_object(Bucket=remote_folder, Key=file_name)
+    logging.info(f"Upload {local_file} ok,object_information: {object_information}")
+
+    # Delete object
+    # s3.delete_object(Bucket=remote_folder, Key=file_name)
+    return get_url(file_name)
+
+
+r"""
+python -m demo.aws.upload r2 podcast audios/podcast/LLM.mp3
+python -m demo.aws.upload img_url LLM.mp3
+"""
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(pathname)s:%(lineno)d - %(funcName)s - %(message)s',
+        handlers=[
+            # logging.FileHandler("content_parser_tts.log"),
+            logging.StreamHandler()
+        ],
+    )
+    app()
---
+++ demo/image_together_flux.py
@@ -0,0 +1,79 @@
+import base64
+import io
+import os
+import logging
+from typing import List
+
+import typer
+from PIL import Image
+from together import Together
+from dotenv import load_dotenv
+
+
+client = Together()
+
+# Load environment variables from .env file
+load_dotenv(override=True)
+
+app = typer.Typer()
+
+
+@app.command('gen_image')
+def gen_image(
+    prompt: str,
+    width: int = 640,
+    height: int = 480,
+    steps: int = 4,
+    n: int = 1,
+) -> List[str]:
+    response = client.images.generate(
+        model="black-forest-labs/FLUX.1-schnell-Free",
+        prompt=prompt,
+        width=width,
+        height=height,
+        steps=steps,
+        n=n,
+        response_format="b64_json"
+    )
+
+    res = []
+    if len(response.data) > 0:
+        res.append(response.data[0].b64_json)
+
+    return res
+
+
+@app.command('save_image')
+def save_gen_image(
+        prompt: str,
+        file_name: str,
+        save_dir: str = "./images",
+        width: int = 640,
+        height: int = 480,
+):
+    base64_imgs = gen_image(prompt, width=width, height=height, steps=4, n=1)
+    if not base64_imgs:
+        raise Exception("no gen image")
+    for base64_img in base64_imgs:
+        img_bytes = io.BytesIO(base64.b64decode(base64_img))
+        img = Image.open(img_bytes)
+        save_path = os.path.join(save_dir, f"{file_name}.png")
+        # img.show()
+        img.save(save_path, "PNG")
+        return save_path
+
+
+r"""
+python -m demo.image_together_flux gen_image "llama, sitting in a field of flowers"
+python -m demo.image_together_flux save_image "llama, sitting in a field of flowers" 123
+"""
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(pathname)s:%(lineno)d - %(funcName)s - %(message)s',
+        handlers=[
+            # logging.FileHandler("content_parser_tts.log"),
+            logging.StreamHandler()
+        ],
+    )
+    app()
