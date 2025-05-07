a02a4f41fdc713dd1d4e268ce700f21585979a03
weedge <weege007@gmail.com>
2025-05-07 17:37:16 +0800

    add whisper_infer_bls (python BE) + whisper_tensorrt_llm_cpprunner (python BE)
    Signed-off-by: weedge <weege007@gmail.com>

---
+++ deploy/modal/src/llm/trtllm/model_repo/README.md
@@ -0,0 +1,4 @@
+# whisper triton inference server
+- whisper (python BE)
+- whisper_infer_bls (python BE) + whisper_tensorrt_llm_cpprunner (python BE)
+- whisper_bls (python BE) + whisper_tensorrt_llm (tensorrtllm BE)
--- deploy/modal/src/llm/trtllm/model_repo/whisper_tensorrt_llm_cpprunner/1/fbank.py
+++ deploy/modal/src/llm/trtllm/model_repo/whisper_tensorrt_llm_cpprunner/1/fbank.py
@@ -12,17 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # Reference: https://github.com/openai/whisper/blob/main/whisper/audio.py
-import os
-from typing import Union
-from functools import lru_cache
-
 import numpy as np
 import torch
 import torch.nn.functional as F
+from typing import Union
+import os
 
-
-@lru_cache(maxsize=None)
-def mel_filters(device, n_mels: int = 128, mel_filters_dir: str = None) -> torch.Tensor:
+def mel_filters(device, n_mels: int =128) -> torch.Tensor:
     """
     load the mel filterbank matrix for projecting STFT into a Mel spectrogram.
     Allows decoupling librosa dependency; saved using:
@@ -32,18 +28,17 @@ def mel_filters(device, n_mels: int = 128, mel_filters_dir: str = None) -> torch
             mel_128=librosa.filters.mel(sr=16000, n_fft=400, n_mels=128),
         )
     """
-    assert n_mels == 80 or n_mels == 128, f"Unsupported n_mels: {n_mels}"
-    if mel_filters_dir is None:
-        mel_filters_path = os.path.join(os.path.dirname(__file__), "mel_filters.npz")
-    else:
-        mel_filters_path = os.path.join(mel_filters_dir, "mel_filters.npz")
-    with np.load(mel_filters_path) as f:
+    assert n_mels == 80 or n_mels == 128 , f"Unsupported n_mels: {n_mels}"
+    with np.load(
+        os.path.join(os.path.dirname(__file__), "mel_filters.npz")
+    ) as f:
         return torch.from_numpy(f[f"mel_{n_mels}"]).to(device)
 
 
 def log_mel_spectrogram(
     audio: Union[torch.Tensor],
     filters: torch.Tensor,
+    n_mels: int = 128,
     n_fft: int = 400,
     hop_length: int = 160,
 ):
@@ -77,26 +72,25 @@ def log_mel_spectrogram(
     log_spec = log_spec.half()
     return log_spec
 
-
 class FeatureExtractor(torch.nn.Module):
     """Your Python model must use the same class name. Every Python model
     that is created must have "TritonPythonModel" as the class name.
     """
 
-    def __init__(self, n_mels: int = 128, mel_filters_dir: str = None):
+    def __init__(self, n_mels: int = 128):
         self.device = torch.device("cuda")
         self.n_mels = n_mels
-        self.mel_filters_dir = mel_filters_dir
-        self.filters = mel_filters(
-            self.device,
-            n_mels=self.n_mels,
-            mel_filters_dir=mel_filters_dir,
-        )
+        self.filters = mel_filters(self.device, n_mels=self.n_mels)
 
-    def compute_feature(self, wav, target: int = 3000):
+    def compute_feature(self, wav, padding_target_len: int = 3000):
+        """
+        Compute the log-Mel spectrogram of the input audio waveform.
+        mel: [1, feature_dim, seq_len]
+        """
         mel = log_mel_spectrogram(wav, self.filters)
-        if mel.shape[1] < target:
-            mel = F.pad(mel, (0, target - mel.shape[1]), mode="constant")
+        assert padding_target_len <= 3000, f"padding must be less than 3000, got {padding_target_len}"
+        if mel.shape[1] < padding_target_len:
+            mel = F.pad(mel, (0, padding_target_len - mel.shape[1]), mode='constant')
         if mel.shape[1] % 2:
             # pad to even length for remove_padding case, since conv1d requires even length
             mel = torch.nn.functional.pad(mel, (0, 1))
